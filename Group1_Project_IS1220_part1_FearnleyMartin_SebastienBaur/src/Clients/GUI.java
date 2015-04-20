2bd36f9b140c7529f03107c78466bedabb0804f8
Sébastien Baur <sebastien.baur@hotmail.fr>
2015-04-20 19:33:12 +0200

    GUI

---
+++ Group1_Project_IS1220_part1_FearnleyMartin_SebastienBaur/src/Clients/GUI.java
@@ -0,0 +1,18 @@
+package Clients;
+
+import javax.swing.JFrame;
+
+import treeImplementation.NotInTreeException;
+import dataTypes.VirtualDisk;
+
+public class GUI extends JFrame{
+
+	public static void main(String[] args) throws NotInTreeException {
+		// TODO Auto-generated method stub
+		Frame frame = new Frame();
+
+
+
+	}
+
+}
