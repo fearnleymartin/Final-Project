e6120eb518d86e7847e0ca73001e03d6340fc431
Fearnley Martin <fearnley.martin@student.ecp.fr>
2015-04-20 20:04:56 +0200

    gui1

---
+++ Group1_Project_IS1220_part1_FearnleyMartin_SebastienBaur/src/Clients/GUI1.java
@@ -0,0 +1,16 @@
+package Clients;
+
+import javax.swing.JFrame;
+
+import treeImplementation.NotInTreeException;
+import dataTypes.VirtualDisk;
+
+public class GUI1 extends JFrame{
+
+	public static void main(String[] args) throws NotInTreeException {
+		// TODO Auto-generated method stub
+	Frame frame = new Frame();
+	
+	}
+
+}
